1628dbb16d1e5afbbfea6f9baf4d7f3833f7f98b
Lam Hoang Tran <s3714110@student.rmit.edu.au>
2021-10-29 15:21:27 +1100

    added duplicate name check

--- init.c
+++ init.c
@@ -2,9 +2,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define ValidFileCheck "NOTES V1.0"
 #define MAX_LINE_LENGTH 255
+
+int duplicate_name(char *name, int start_line, char *filename)
+{
+    int is_duplicate = 0;
+    int line = start_line;
+    FILE *temp_file;
+    char temp_line[MAX_LINE_LENGTH] = {0};
+
+    if ((temp_file = fopen(filename, "r")))
+    {
+        for (int i = 1; i <= line; i++)
+        {
+            fgets(temp_line, MAX_LINE_LENGTH, temp_file);
+        }
+
+        while (fgets(temp_line, MAX_LINE_LENGTH, temp_file))
+        {
+            line++;
+            temp_line[strcspn(temp_line, "\n")] = '\0';
+
+            if (strcmp(name, temp_line) == 0)
+            {
+                is_duplicate = 1;
+                fprintf(stderr, "Duplicate names found at line %d and line %d !\n", start_line, line);
+            }
+        }
+
+        fclose(temp_file);
+    }
+
+    return is_duplicate;
+}
 
 int init(char *filename)
 {
@@ -29,16 +62,29 @@ int init(char *filename)
             printf("Note file identifier detected. Checking for file validity...\n");
             while (fgets(next_line, MAX_LINE_LENGTH, file))
             {
-                if ( next_line[0] == '@' )
+                line_count++;
+                next_line[strcspn(next_line, "\n")] = '\0';
+
+                if (next_line[0] == '@')
+                {
+                    if (duplicate_name(next_line, line_count, filename) == 1)
+                    {
+                        fprintf(stderr, "This is not a valid notes file. Please remove the notes file and run the program again\n");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                else if (next_line[0] == '=')
+                {
+                    if (duplicate_name(next_line, line_count, filename) == 1)
+                    {
+                        fprintf(stderr, "This is not a valid notes file. Please remove the notes file and run the program again\n");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                else if (next_line[0] == '#')
                 {
                 }
-                else if ( next_line[0] == '=' )
-                {
-                }
-                else if ( next_line[0] == '#' )
-                {
-                }
-                else if ( next_line[0] == ' ' )
+                else if (next_line[0] == ' ')
                 {
                 }
                 else
--- init.h
+++ init.h
@@ -1,1 +1,2 @@
+int duplicate_name(char *name, int start_line, char *filename);
 int init(char *filename);
