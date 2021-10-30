d4a145502447756ca9dec83b6141d6021d1ac0ae
Lam Hoang Tran <s3714110@student.rmit.edu.au>
2021-10-31 04:10:08 +1100

    added check exist func for rm

--- rm.c
+++ rm.c
@@ -4,6 +4,33 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define BufferFileName "temp_buffer.notes"
+#define MAX_LINE_LENGTH 255
+
+int check_exist(char *name, char *filename)
+{
+    int exists = 0;
+    FILE *temp_file;
+    char temp_line[MAX_LINE_LENGTH] = {0};
+
+    if ((temp_file = fopen(filename, "r")))
+    {
+        while (fgets(temp_line, MAX_LINE_LENGTH, temp_file))
+        {
+            temp_line[strcspn(temp_line, "\n")] = '\0';
+
+            if (strcmp(name, temp_line) == 0)
+            {   
+                exists = 1;
+            }
+        }
+
+        fclose(temp_file);
+    }
+
+    return exists;
+}
+
 int remove_file(char *filename, char *if_name)
 {
 
--- rm.h
+++ rm.h
@@ -1,2 +1,3 @@
+int check_exist(char *name, char *file_name);
 int remove_file(char *filename, char *if_name);
 int remove_dir(char *filename, char *id_name);
