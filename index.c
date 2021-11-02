db4e480b453e8f80a0b74216e3b9bcfa28ff0aa7
Lam Hoang Tran <s3714110@student.rmit.edu.au>
2021-11-02 22:29:30 +1100

    added comments for index

--- index.c
+++ index.c
@@ -5,6 +5,9 @@
 #include <stdbool.h>
 #include <sysexits.h>
 
+//index file has the format:
+//first line: file name or dir name with same leading characters as in FS file
+//second line: the line of that file or dir in the FS file with a space leading character
 #define IndexFileExtension ".index"
 #define MAX_LINE_LENGTH 255
 
@@ -13,15 +16,16 @@ int create_index(char *filename)
     char index_filename[MAX_LINE_LENGTH] = {0};
     strncpy(index_filename, filename, strlen(filename));
 
-    strcat(index_filename, IndexFileExtension);
+    strcat(index_filename, IndexFileExtension); //concats file extention to create name for the index file
 
     FILE *file;
     FILE *index_file;
-    int line_count = 0;
+    int line_count = 0; // counts the line
 
     if ((file = fopen(filename, "r")) && (index_file = fopen(index_filename, "w+")))
     {
         char next_line[MAX_LINE_LENGTH] = {0};
+        // goes throught FS file line by line
         while (fgets(next_line, MAX_LINE_LENGTH, file))
         {
             char next_index_line[MAX_LINE_LENGTH] = {0};
@@ -29,8 +33,9 @@ int create_index(char *filename)
             if (next_line[0] == '@')
             {
                 char line_count_str[MAX_LINE_LENGTH];
-                sprintf(line_count_str, "%d", line_count);
+                sprintf(line_count_str, "%d", line_count); // converts int to char array
 
+                // writes file name and its line number into index file
                 fputs(next_line, index_file);
                 next_index_line[0] = ' ';
                 strcat(next_index_line, line_count_str);
@@ -40,8 +45,9 @@ int create_index(char *filename)
             else if (next_line[0] == '=')
             {
                 char line_count_str[MAX_LINE_LENGTH];
-                sprintf(line_count_str, "%d", line_count);
+                sprintf(line_count_str, "%d", line_count);  // converts int to char array
 
+                // writes dir name and its line number into index file
                 fputs(next_line, index_file);
                 next_index_line[0] = ' ';
                 strcat(next_index_line, line_count_str);
@@ -62,6 +68,7 @@ int create_index(char *filename)
     return EXIT_SUCCESS;
 }
 
+// lists out the index file to stdout
 int read_index(char *filename)
 {
     char index_filename[MAX_LINE_LENGTH] = {0};
